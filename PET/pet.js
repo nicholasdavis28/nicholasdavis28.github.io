9ef3aa6cb0650d7230a43cb6fa7be25fe82caef1
nicholasdavis28 <ndavis@eastsideprep.org>
2022-01-27 00:09:41 -0800

    main

---
+++ PET/pet.js
@@ -0,0 +1,15 @@
+let food = 0;
+let money = 0;
+let beer = 0;
+
+function foodLeprechaun() {
+    console.log("Feeding Leprechaun")
+}
+
+function moneyLeprechaun() {
+    console.log("Paying Leprechaun")
+}
+
+function beerLeprechaun() {
+    console.log("Drinking Leprechaun")
+}
