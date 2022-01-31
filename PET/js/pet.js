2f4a900845a7278f8188bf6e089c37530fd60f64
nicholasdavis28 <ndavis@eastsideprep.org>
2022-01-30 23:24:04 -0800

    PET

--- PET/js/pet.js
+++ PET/js/pet.js
@@ -16,10 +16,26 @@ function foodLeprechaun() {
 
 function moneyLeprechaun() {
     console.log("Paying Leprechaun")
+    if (money >= 100) {
+        money += 0
+    } else {
+        money += 10
+    }
+    console.log("money: " + money)
+
+    refreshUI();
 }
 
 function beerLeprechaun() {
     console.log("Drinking Leprechaun")
+    if (beer >= 100) {
+        beer += 0
+    } else {
+        beer += 10
+    }
+    console.log("beer: " + beer)
+
+    refreshUI();
 }
 
 function refreshUI() {
@@ -27,4 +43,38 @@ function refreshUI() {
     foodMeter.value = food;
     let foodParagraph = document.getElementById("food-paragraph")
     foodParagraph.innerHTML = food;
+    let moneyMeter = document.getElementById("money-meter");
+    moneyMeter.value = money;
+    let moneyParagraph = document.getElementById("money-paragraph")
+    moneyParagraph.innerHTML = money;
+    let beerMeter = document.getElementById("beer-meter");
+    beerMeter.value = beer;
+    let beerParagraph = document.getElementById("beer-paragraph")
+    beerParagraph.innerHTML = beer;
+}
+
+myInterval = setInterval(foodTime, 5000)
+myInterval = setInterval(moneyTime, 5000)
+myInterval = setInterval(beerTime, 5000)
+
+
+function foodTime() {
+    if (food > 0) {
+        food = food - 10
+    }
+    refreshUI();
+}
+
+function moneyTime() {
+    if (money > 0) {
+        money = money - 10
+    }
+    refreshUI();
+}
+
+function beerTime() {
+    if (beer > 0) {
+        beer = beer - 10
+    }
+    refreshUI();
 }
